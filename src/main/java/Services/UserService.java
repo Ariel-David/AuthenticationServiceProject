239545a1a0b486f5ad457ed550396acdc523298c
Ariel <arieldavid642@gmail.com>
2022-10-30 16:23:02 +0200

    UserService ia added

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -1,5 +1,25 @@
 package Services;
 
+import Controllers.UserController;
+import DataSource.Repo;
+import be.User;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class UserService {
 
+    public void validUniqueEmail(String email){
+        if(Repo.getInstance().getUserByEmail(email) != null){
+            throw new IllegalArgumentException("A user with this email already exists. Choose another email.");
+        }
+    }
+
+    public User createUser(String name, String email,String password){
+        User u = new User(name,email,password);
+        Repo.getInstance().addNewUser(u);
+        System.out.println("User is created!");
+        return u;
+    }
 }
