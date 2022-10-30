b002b023671b58f2879680a8dcd25409ba8a5844
hadi6645 <hadi6645@gmail.com>
2022-10-30 21:06:12 +0200

    add new user to repo function and pom dependency

--- src/main/java/DataSource/Repo.java
+++ src/main/java/DataSource/Repo.java
@@ -1,8 +1,14 @@
 package DataSource;
 
 import be.User;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Repo {
+    static final String PATH = "src/main/java/DataSource/jsons";
 
     private static Repo instance;
 
@@ -17,7 +23,18 @@ public class Repo {
     }
 
     public void addNewUser(User user) {
-
+        Gson gson = new Gson();
+        int userId = user.getId();
+        File userJsonFile = new File(PATH+"User"+userId);
+        try {
+            userJsonFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("User file creation failed");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        String userJson = gson.toJson(user);
+        //write to json
         throw new UnsupportedOperationException();
     }
 
