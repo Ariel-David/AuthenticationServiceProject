70567531099687e382d691f3f04f57146de6c7ef
Ariel <arieldavid642@gmail.com>
2022-10-30 21:57:01 +0200

    Add validation class

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -10,16 +10,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class UserService {
 
-    public void validUniqueEmail(String email){
-        if(Repo.getInstance().getUserByEmail(email) != null){
+    public void validUniqueEmail(String email) {
+        if (Repo.getInstance().getUserByEmail(email) != null) {
             throw new IllegalArgumentException("A user with this email already exists. Choose another email.");
         }
     }
 
-    public User createUser(String name, String email,String password){
-        User u = new User(name,email,password);
+    public User createUser(String name, String email, String password) {
+        User u = new User(name, email, password);
         Repo.getInstance().addNewUser(u);
         System.out.println("User is created!");
         return u;
+    }
+
+    public void changePassword(User u, String password) {
+        u.setPassword(password);
+    }
+
+    public void changeName(User u, String name) {
+        u.setPassword(name);
     }
 }
--- src/main/java/Services/Validation.java
+++ src/main/java/Services/Validation.java
@@ -1,2 +1,24 @@
-package Services;public class Validation {
+package Services;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public boolean validPassword(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can't be empty!");
+        } else if (password.length() < 8 && password.length() > 12) {
+            throw new IllegalArgumentException("Password length invalid");
+        }
+        return true;
+    }
+
+    public boolean validEmail(String email) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return matcher.find();
+    }
+
 }
