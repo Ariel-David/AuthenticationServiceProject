46c0f303a08a5ee42cc4d8170a6c23a199ae85d4
itamarit <itamarit124812@gmail.com>
2022-10-30 23:01:20 +0200

    finish user

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,4 +1,7 @@
 package Client;
+
+import java.util.Scanner;
+
 enum UserActions{
     REGISTER,
     LOGIN,
@@ -9,24 +12,65 @@ enum UserActions{
 public class Client {
     private String token;
 
+
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         UserActions action;
         while(true)
         {
-            action=System.in.read();
+            System.out.println("please choose one of the following options:");
+            int i=0;
+            for (var act:UserActions.values())
+            {
+                System.out.printf("%d: %s\n",++i,act);
+            }
+            action= UserActions.valueOf(scanner.next());
             switch (action) {
                 case REGISTER:
+                    handelRegister(scanner);
                     break;
                 case LOGIN:
-                    break;
+                    handelLogin(scanner);
                 case UPDATE_NAME:
-                    break;
+                    HandelUpdateName(scanner);
                 case UPDATE_PASSWORD:
+                    HandelUpdatePassword(scanner);
                     break;
                 case EXIT:
-                    break;
+                    return;
             }
         }
 
+    }
+    private static void HandelUpdatePassword(Scanner scanner) {
+        System.out.println("enter your token:");
+        String token=scanner.nextLine();
+        System.out.println("enter your new password:");
+        String name=scanner.nextLine();
+    }
+
+    private static void HandelUpdateName(Scanner scanner) {
+        System.out.println("enter your token:");
+        String token=scanner.nextLine();
+        System.out.println("enter your new name:");
+        String name=scanner.nextLine();
+    }
+
+    private static void handelLogin(Scanner scanner) {
+        System.out.println("enter your email:");
+        String email= scanner.nextLine();
+        System.out.println("enter your password:");
+        String password= scanner.nextLine();
+
+    }
+
+    private static void handelRegister(Scanner scanner) {
+        System.out.println("    Start Registration:   ");
+        System.out.println("enter your name:");
+        String name= scanner.nextLine();
+        System.out.println("enter a valid email:");
+        String email= scanner.nextLine();
+        System.out.println("enter a password:");
+        String password= scanner.nextLine();
     }
 }
