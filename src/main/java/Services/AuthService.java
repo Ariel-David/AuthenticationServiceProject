cb3dbec2255b8fe010daef7bd0ab2caa847ed0ac
itamarit <itamarit124812@gmail.com>
2022-10-30 15:26:30 +0200

    Add Auth Service

--- src/main/java/Services/AuthService.java
+++ src/main/java/Services/AuthService.java
@@ -1,4 +1,25 @@
 package Services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthService {
+    public static AuthService getInstance()
+    {
+
+    }
+    private  static A
+    private Map<Integer,String> Tokens;
+    private AuthService()
+    {
+        Tokens=new HashMap<>();
+    }
+    private String createNewToken(String email)
+    {
+
+    }
+    public boolean login(String email,String password)
+    {
+
+    }
 }
