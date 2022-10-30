13c6de460b19ce638039953860505c5d30c6fea1
itamarit <itamarit124812@gmail.com>
2022-10-30 16:05:16 +0200

    Add singelton to Autoservice  and make login

--- src/main/java/Services/AuthService.java
+++ src/main/java/Services/AuthService.java
@@ -1,14 +1,18 @@
 package Services;
 
+import DataSource.Repo;
+import be.User;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class AuthService {
     public static AuthService getInstance()
     {
-
+       if(instance==null) instance= new AuthService();
+       return  instance;
     }
-    private  static A
+    private  static AuthService instance;
     private Map<Integer,String> Tokens;
     private AuthService()
     {
@@ -16,10 +20,16 @@ public class AuthService {
     }
     private String createNewToken(String email)
     {
-
+        return
     }
     public boolean login(String email,String password)
     {
+        Repo repo=Repo.getInstance();
+        User userByEmail = repo.getUserByEmail(email);
+        if(userByEmail==null) throw new IllegalArgumentException("There is no user with the email you type");
+        if(userByEmail.getPassword().equals(password)) {
 
+        }
+        return false;
     }
 }
