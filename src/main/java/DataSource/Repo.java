7873b0aedeee118dcf4c60f3eb8523500aa454e8
hadi6645 <hadi6645@gmail.com>
2022-10-30 15:40:27 +0200

    added repo singleton and gitignore

--- src/main/java/DataSource/Repo.java
+++ src/main/java/DataSource/Repo.java
@@ -3,9 +3,22 @@ package DataSource;
 import be.User;
 
 public class Repo {
+
+    private static Repo instance;
+    public static Repo getInstance()
+    {
+        if(instance == null){
+            instance = new Repo();
+        }
+        return instance;
+    }
+    private Repo() {
+    }
+
     public void addNewUser(User user)
     {
-      throw  new UnsupportedOperationException();
+
+        throw  new UnsupportedOperationException();
     }
 
     public User getUserByID(int id)
