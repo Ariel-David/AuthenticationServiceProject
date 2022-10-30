6aef736eea4fc3fa19d4be912e93bfc0128b0e08
hadi6645 <hadi6645@gmail.com>
2022-10-30 15:42:26 +0200

    reformat repo file

--- src/main/java/DataSource/Repo.java
+++ src/main/java/DataSource/Repo.java
@@ -5,29 +5,28 @@ import be.User;
 public class Repo {
 
     private static Repo instance;
-    public static Repo getInstance()
-    {
-        if(instance == null){
+
+    private Repo() {
+    }
+
+    public static Repo getInstance() {
+        if (instance == null) {
             instance = new Repo();
         }
         return instance;
     }
-    private Repo() {
+
+    public void addNewUser(User user) {
+
+        throw new UnsupportedOperationException();
     }
 
-    public void addNewUser(User user)
-    {
-
-        throw  new UnsupportedOperationException();
+    public User getUserByID(int id) {
+        throw new UnsupportedOperationException();
     }
 
-    public User getUserByID(int id)
-    {
-        throw  new UnsupportedOperationException();
-    }
-    public User getUserByEmail(String email)
-    {
-        throw  new UnsupportedOperationException();
+    public User getUserByEmail(String email) {
+        throw new UnsupportedOperationException();
     }
 
 }
