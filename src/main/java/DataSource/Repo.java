6c8f2a21a53757bda0546d1f0e50d0e01ad4f636
Ariel <arieldavid642@gmail.com>
2022-10-31 09:15:47 +0200

    Add some function to Repo and to User

--- src/main/java/DataSource/Repo.java
+++ src/main/java/DataSource/Repo.java
@@ -6,15 +6,22 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class Repo {
     static final String PATH = "src/main/java/DataSource/jsons";
-
+    private Map<Integer, User> users;
     private static Repo instance;
 
     private Repo() {
     }
 
+    public void updateUser(User user){
+        if(users.containsKey(user.getId())){
+            users.put(user.getId(), user);
+        }
+    }
+    
     public static Repo getInstance() {
         if (instance == null) {
             instance = new Repo();
@@ -25,7 +32,7 @@ public class Repo {
     public void addNewUser(User user) {
         Gson gson = new Gson();
         int userId = user.getId();
-        File userJsonFile = new File(PATH+"User"+userId);
+        File userJsonFile = new File(PATH + "User" + userId);
         try {
             userJsonFile.createNewFile();
         } catch (IOException e) {
